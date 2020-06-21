307db3ec5d57392dd885eeff8c4912ddf2937f8c
ManalMahmoudGouda <39454638+ManalMahmoudGouda@users.noreply.github.com>
2020-06-21 02:02:41 +0200

    Move Extent Report To Test Base Make OS and Report path Configurable

--- Vois-Automation-Task/src/test/java/tests/Search.java
+++ Vois-Automation-Task/src/test/java/tests/Search.java
@@ -31,38 +31,12 @@ import java.util.concurrent.TimeUnit;
 public class Search extends TestBase {
     protected JSONObject searchTestData;
     GoogleSearchPage googleSearchPage;
-    ExtentHtmlReporter htmlReporter;
-    ExtentReports extent;
-    //helps to generate the logs in test report.
-    ExtentTest logger;
 
     public Search() throws IOException, ParseException {
         searchTestData = (JSONObject) JSONReader.readJson("src/test/resources/test-data/test-data.json");
 
     }
 
-    @BeforeTest
-    public void startReport() {
-        // initialize the HtmlReporter
-        htmlReporter = new ExtentHtmlReporter(System.getProperty("user.dir") + "/test-report/testReport.html");
-
-        //initialize ExtentReports and attach the HtmlReporter
-        extent = new ExtentReports();
-        extent.attachReporter(htmlReporter);
-
-        //To add system or environment info by using the setSystemInfo method.
-        extent.setSystemInfo("OS", "Windows");
-        extent.setSystemInfo("Browser", "Chrome");
-
-        //configuration items to change the look and feel
-        //add content, manage tests etc
-        htmlReporter.config().setChartVisibilityOnOpen(true);
-        htmlReporter.config().setDocumentTitle("Extent Report Demo");
-        htmlReporter.config().setReportName("Test Report");
-        htmlReporter.config().setTestViewChartLocation(ChartLocation.TOP);
-        htmlReporter.config().setTheme(Theme.STANDARD);
-        htmlReporter.config().setTimeStampFormat("EEEE, MMMM dd, yyyy, hh:mm a '('zzz')'");
-    }
 
     @Test
     public void searchVodafone_TestCase() throws IOException, ParseException, InterruptedException {
@@ -70,7 +44,7 @@ public class Search extends TestBase {
 
         driver.navigate().to((String) jsonConfig.get("applicationURL"));
         //put in variable
-        logger.log(Status.INFO, "Navigated to "+jsonConfig.get("applicationURL")+ " successfully ");
+        logger.log(Status.INFO, "Navigated to " + jsonConfig.get("applicationURL") + " successfully ");
         // initialize google page
         googleSearchPage = new GoogleSearchPage(driver);
 
@@ -85,7 +59,7 @@ public class Search extends TestBase {
         logger.log(Status.INFO, "Scroll Down until 'Next' link appear");
         googleSearchPage.scrollDownOfPage(googleSearchPage.getNextAnchor());
         googleSearchPage.waitUntilVisible(googleSearchPage.getNextAnchor());
-        Assert.assertTrue(googleSearchPage.getNextAnchor().isEnabled(),"Next link not Clickable");
+        Assert.assertTrue(googleSearchPage.getNextAnchor().isEnabled(), "Next link not Clickable");
         logger.log(Status.PASS, "Scroll Down and next link appear successfully");
 
         logger.log(Status.INFO, "Click on Next Link to navigate to page 2");
@@ -99,7 +73,6 @@ public class Search extends TestBase {
         int countOfSecondPage = PageBase.countSearchResult(googleSearchPage.getSearchResultList());
         googleSearchPage.scrollDownOfPage(googleSearchPage.getNextAnchor());
         googleSearchPage.clickBtn(googleSearchPage.getNextAnchor());
-        this.IsNextLinkEnabled();
         Assert.assertTrue(googleSearchPage.IsElementTextContains(googleSearchPage.getResultPageInfo(), (String) searchTestData.get("thirdPage")));
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         int countOfThirdPage = PageBase.countSearchResult(googleSearchPage.getSearchResultList());
@@ -107,13 +80,6 @@ public class Search extends TestBase {
 
     }
 
-    public void IsNextLinkEnabled() throws InterruptedException {
-        if (googleSearchPage.getNextAnchor().isEnabled()) {
-            logger.log(Status.PASS, "Scroll Down and click on the next link");
-        } else {
-            logger.log(Status.FAIL, "Failed to verfie");
-        }
-    }
 
     @AfterMethod
     public void getResult(ITestResult result) throws Exception {
@@ -124,34 +90,8 @@ public class Search extends TestBase {
             logger.log(Status.FAIL, MarkupHelper.createLabel(result.getName() + " FAILED ", ExtentColor.RED));
         } else if (result.getStatus() == ITestResult.SUCCESS) {
             logger.log(Status.PASS, MarkupHelper.createLabel(result.getName() + " PASSED ", ExtentColor.GREEN));
-        } else {
-            logger.log(Status.SKIP, MarkupHelper.createLabel(result.getName() + " SKIPPED ", ExtentColor.ORANGE));
-            logger.skip(result.getThrowable());
         }
     }
 
-    private String getScreenshot(String screenshotName) throws Exception {
-        //below line is just to append the date format with the screenshot name to avoid duplicate names
-        String currentTimeStamp = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
-        String filename = screenshotName + currentTimeStamp + ".png";
 
-
-        TakesScreenshot ts = (TakesScreenshot) driver;
-        File source = ts.getScreenshotAs(OutputType.FILE);
-
-        //after execution, you could see a folder "FailedTestsScreenshots" under src folder
-        String destination = System.getProperty("user.dir") + "/test-report/" + filename;
-        File finalDestination = new File(destination);
-        FileUtils.copyFile(source, finalDestination);
-
-        //Returns the captured file path
-        return filename;
-    }
-
-    @AfterTest
-    public void tearDown() {
-        //to write or update test information to reporter
-        extent.flush();
-        //  driver.quit();
-    }
 }
--- Vois-Automation-Task/src/test/java/tests/TestBase.java
+++ Vois-Automation-Task/src/test/java/tests/TestBase.java
@@ -1,6 +1,12 @@
 package tests;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.aventstack.extentreports.reporter.configuration.ChartLocation;
+import com.aventstack.extentreports.reporter.configuration.Theme;
 import common.JSONReader;
+import org.apache.commons.io.FileUtils;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.ParseException;
 import org.openqa.selenium.*;
@@ -15,15 +21,23 @@ import org.openqa.selenium.ie.InternetExplorerOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 public abstract class TestBase {
     public static WebDriver driver = null;
     protected JSONObject jsonConfig;
+   private ExtentHtmlReporter htmlReporter;
+   protected ExtentReports extent;
+    //helps to generate the logs in test report.
+     protected  ExtentTest logger;
+
 
     public TestBase() {
     }
@@ -38,10 +52,11 @@ public abstract class TestBase {
             case "FF":
                 System.setProperty("webdriver.gecko.driver", (String) jsonConfig.get("fireFoxDriverPath"));
                 FirefoxProfile ffProfile = new FirefoxProfile();
-                ffProfile.setPreference("javascript.enabled", false);
+               // ffProfile.setPreference("javascript.enabled", false);
+                ffProfile.setPreference("intl.accept_languages", "en-GB");
 
                 FirefoxOptions ffOptions = new FirefoxOptions();
-                ffOptions.setLegacy(true);
+//                ffOptions.setLegacy(true);
                 ffOptions.setProfile(ffProfile);
                 driver = new FirefoxDriver(ffOptions);
                 break;
@@ -67,13 +82,54 @@ public abstract class TestBase {
         }
 
         driver.manage().window().maximize();
+        this.startReport();
+    }
+
+    private void startReport() {
+        // initialize the HtmlReporter
+        htmlReporter = new ExtentHtmlReporter(System.getProperty("user.dir")
+                + jsonConfig.get("extentReportPath")+"testReport.html");
+
+        //initialize ExtentReports and attach the HtmlReporter
+        extent = new ExtentReports();
+        extent.attachReporter(htmlReporter);
+
+        //To add system or environment info by using the setSystemInfo method.
+        extent.setSystemInfo("OS", (String) jsonConfig.get("osValue"));
+        extent.setSystemInfo("Browser",(String)jsonConfig.get("browserToBeUsed"));
+
+        //configuration items to change the look and feel
+        //add content, manage tests etc
+        htmlReporter.config().setChartVisibilityOnOpen(true);
+        htmlReporter.config().setDocumentTitle("Test Report");
+        htmlReporter.config().setReportName("Test Report");
+        htmlReporter.config().setTestViewChartLocation(ChartLocation.TOP);
+        htmlReporter.config().setTheme(Theme.STANDARD);
+        htmlReporter.config().setTimeStampFormat("EEEE, MMMM dd, yyyy, hh:mm a '('zzz')'");
+    }
+    protected String getScreenshot(String screenshotName) throws Exception {
+        //below line is just to append the date format with the screenshot name to avoid duplicate names
+        String currentTimeStamp = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+        String filename = screenshotName + currentTimeStamp + ".png";
+
+
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        File source = ts.getScreenshotAs(OutputType.FILE);
+
+        //after execution, you could see a folder "FailedTestsScreenshots" under src folder
+        String destination = System.getProperty("user.dir") +   jsonConfig.get("extentReportPath") + filename;
+        File finalDestination = new File(destination);
+        FileUtils.copyFile(source, finalDestination);
+
+        //Returns the captured file path
+        return filename;
     }
 
     //Test cleanup
     @AfterClass
     public void TeardownTest() {
-
-        // TestBase.driver.quit();
+        extent.flush();
+        driver.quit();
     }
 
 
